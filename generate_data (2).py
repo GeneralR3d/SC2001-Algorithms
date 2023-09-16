d359099b901c4f102e7d5d90a43e3f2dde203888
TdrDingren <97043423+TdrDingren@users.noreply.github.com>
2023-09-16 13:48:45 +0800

    added files from antonio

--- generate_data (2).py
+++ generate_data (2).py
@@ -1,7 +1,7 @@
 import numpy as np
 
 
-def generate_data(start_size=1000, end_size=1000000, x=101, seed=None):
+def generate_data(start_size=1000, end_size=1000000, max_int=101, seed=None):
     if seed is not None:
         np.random.seed(seed)
 
@@ -10,7 +10,7 @@ def generate_data(start_size=1000, end_size=1000000, x=101, seed=None):
     # we just want to take one representative value for each order of magnitude
     for exponent in range(int(np.log10(start_size)), int(np.log10(end_size)) + 1):
         size = 10 ** exponent
-        data = np.random.randint(1, x, size)
+        data = np.random.randint(1, max_int, size)
         data_sizes.append(data)
 
     return data_sizes
--- sorting_algorithms.py
+++ sorting_algorithms.py
@@ -47,3 +47,35 @@ class HybridSort:
         right = self.sort(arr[mid:], threshold)
 
         return self.merge(left, right)
+
+
+class MergeSort:
+    def __init__(self):
+        self.comparison_count = 0
+
+    def merge(self, left, right):
+        result = []
+        i = j = 0
+
+        while i < len(left) and j < len(right):
+            self.comparison_count += 1
+            if left[i] < right[j]:
+                result.append(left[i])
+                i += 1
+            else:
+                result.append(right[j])
+                j += 1
+
+        result.extend(left[i:])
+        result.extend(right[j:])
+        return np.array(result)
+
+    def sort(self, arr):
+        if len(arr) <= 1:
+            return arr
+
+        mid = len(arr) // 2
+        left = self.sort(arr[:mid])
+        right = self.sort(arr[mid:])
+
+        return self.merge(left, right)
